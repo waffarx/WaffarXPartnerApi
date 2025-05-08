758023f00c87bd26e539c412906a16346ca61be9
Mahmoud Elbakly <m.abdalltif.elbakly@gmail.com>
2025-05-08 16:34:09 +0300

    edits

--- src/Domain/Entities/NoSqlEntities/FeaturedProductSettingAudit.cs
+++ src/Domain/Entities/NoSqlEntities/FeaturedProductSettingAudit.cs
@@ -6,13 +6,10 @@ public class FeaturedProductSettingAudit
 {
     [BsonId]
     public ObjectId Id { get; set; }
-
     public int CreatedBy { get; set; }
-
     public DateTime CreatedAt { get; set; }
-
     public int ClientApiId { get; set; }
-
+    public string Type { get; set; }
     [BsonElement("FeaturedProductSetting")]
     public FeaturedProductSetting OriginalDocument { get; set; }
 }
