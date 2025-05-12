a9d8252ee7b03b15cf8d1b18f1d75a5d4c2a9497
moatazyassin <moataz@waffarx.com>
2025-05-12 11:48:04 +0300

    add is active to search

--- src/Domain/Models/PartnerSqlModels/UserSearchModel.cs
+++ src/Domain/Models/PartnerSqlModels/UserSearchModel.cs
@@ -4,5 +4,6 @@ public class UserSearchModel
     public string UserId { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
+    public bool IsActive { get; set; }
     public List<string> Teams { get; set; }
 }
