c2125e32d7f953c4fc7dcbeaa1e02ba228724417
moatazyassin <moataz@waffarx.com>
2025-05-13 15:33:30 +0300

    add validation

---
+++ src/Application/Common/DTOs/Dashboard/Products/DeleteFeatured/DeleteFeaturedProductDtoValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace WaffarXPartnerApi.Application.Common.DTOs.Dashboard.Products.DeleteFeatured;
+public class DeleteFeaturedProductDtoValidator : AbstractValidator<DeleteFeaturedProductDto>
+{
+    public DeleteFeaturedProductDtoValidator()
+    {
+        RuleFor(x => x.ProductId)
+            .NotEmpty()
+            .WithMessage("Product ID is required.");
+    }
+}
